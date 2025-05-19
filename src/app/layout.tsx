70b0edf81b07dd2ecedc99e09d93735c3f6ef9b8
harry pattani <harrypattani15@gmail.com>
2025-05-19 14:32:25 +0530

    video 3/9 music site projct only , hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-center`}
+        className={`${geistSans.variable} ${geistMono.variable} `}
       >
-        <div className="relative w-full flex item-cetner justify-center">
+        <div className="relative w-full flex items-center justify-center">
         <Navbar/>
         </div>
         {children}
